c9217ac8738339574f976c5d491c6086fa9589c2
r.ehdals <57896501+do4ng@users.noreply.github.com>
2023-01-12 23:26:28 +0900

    style: add some comments

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,18 @@ import 'colors';
 import { argsParser } from './parser';
 
 export interface Options {
+  /**
+   * ```js
+   * // strict on
+   * const app = program('my-app', { strict: true });
+   * app.option("--bundle, -b", "...")
+   * app.command("build").action((options) => {
+   *  console.log(options.bundle)
+   * })
+   * // $ my-app build --bundle => true
+   * // $ my-app build -b => true
+   * // $ my-app build -B => false
+   */
   strict?: boolean;
 }
 
@@ -32,7 +44,9 @@ class Program {
 
   constructor(name: string, opts?: Options) {
     this.__name = name;
+
     this.__cfg = opts || {};
+
     this.commands = [
       {
         command: '__default',
@@ -43,30 +57,33 @@ class Program {
         alias: [],
       },
     ];
+
     this.__options = {
       help: ['help', 'h'],
       version: ['version', 'v'],
     };
+
     this.__optionsDetails = {
       help: { des: 'Displays help information.', raw: '--help, -h' },
       version: { des: 'Displays current version.', raw: '--version, -v' },
     };
+
     this.__optionsDefault = {};
   }
 
   name(name: string) {
     this.__name = name;
-
     return this;
   }
 
   version(version: string) {
     this.__version = version;
-
     return this;
   }
 
   option(opt: string, description: string, defaultValue: any = undefined) {
+    // parse options
+
     const sliced = opt
       .split(',')
       .map((s) => s.trim())
@@ -77,6 +94,8 @@ class Program {
         if (slice.startsWith('-')) return slice.slice(1);
         return slice;
       });
+
+    // save
 
     this.__options[sliced[0]] = sliced;
     this.__optionsDetails[sliced[0]] = {
@@ -117,46 +136,68 @@ class Program {
     return this;
   }
 
+  /**
+   * describe specific command
+   * @param des
+   * @returns
+   */
   describe(des: string) {
     const c = this.commands.filter((cmd) => cmd.command === this.currentCommand)[0] || 0;
 
-    // default
     if (c === 0) {
+      // default
       this.commands[0].des = des;
     } else {
+      // action
       c.des = des;
     }
 
     return this;
   }
 
+  /**
+   * add a callback to the specific command
+   * @param action Handler
+   * @returns
+   */
   action(action: Function) {
     const c = this.commands.filter((cmd) => cmd.command === this.currentCommand)[0] || 0;
 
-    // default
     if (c === 0) {
+      // default
       this.commands[0].action = action;
     } else {
+      // action
       c.action = action;
     }
 
     return this;
   }
 
+  /**
+   * print help
+   * @param command command for more information
+   * @returns this
+   */
   showHelp(command?: string) {
     const $ = '$'.gray;
     if (!command) {
       let maxCommand = 0;
+
       this.commands.forEach((command) => {
         if (command.command.length > maxCommand) {
           maxCommand = command.command.length;
         }
       });
+
       let maxOption = 0;
+
       Object.keys(this.__options).forEach((opt) => {
         const t = this.__optionsDetails[opt].raw;
         if (t.length > maxOption) maxOption = t.length;
       });
+
+      // print
 
       console.log();
       console.log('  Usage'.bold);
@@ -192,14 +233,19 @@ class Program {
       console.log(`  Run \`${this.__name} <command> --help\` for more information.`);
     } else {
       let maxOption = 0;
+
       Object.keys(this.__options).forEach((opt) => {
         const t = this.__optionsDetails[opt].raw;
         if (t.length > maxOption) maxOption = t.length;
       });
 
+      // find command
+
       const c = this.commands.filter((cmd) => cmd.command === command)[0];
 
       if (!c) return this;
+
+      // print
 
       console.log();
       console.log('  Usage'.bold);
@@ -234,17 +280,31 @@ class Program {
     console.log();
   }
 
+  /**
+   * print current version
+   */
   showVersion() {
     console.log(`${'$'.gray} ${`v${this.__version}`}`);
   }
 
+  /**
+   * parse args and run application
+   * ```js
+   * program.parse(process.argv);
+   * ```
+   * @param args argv
+   */
   parse(args: string[]) {
+    // parse args
+
     const parsed = argsParser(args.slice(2), {
       names: { ...this.__options, help: 'h', version: 'v' },
       strict: this.__cfg.strict || true,
     });
 
     let cmd = parsed.__[0] || '';
+
+    // find command
 
     const c =
       this.commands.filter((f) => {
@@ -256,21 +316,28 @@ class Program {
         return false;
       })[0] || 0;
 
+    // --help, -h
+
     if (parsed.help === true) {
       this.showHelp(cmd);
-
       return this;
     }
+
+    // --version, -v
+
     if (parsed.version === true) {
       this.showVersion();
-
       return this;
     }
+
+    // default
     // eslint-disable-next-line no-unused-expressions
     if (c === 0) this.commands[0].action && this.commands[0].action();
+    // run~!
     else {
       const giveArgs = parsed.__.slice(1);
       const options = { ...this.__optionsDefault, ...parsed };
+
       c.action(...giveArgs, options);
     }
 
